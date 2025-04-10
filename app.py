b9cbca8d425d1c2356682956113d5be1ddec157d
Dev-Pratham <pratham.chaurasia31@gmail.com>
2025-04-10 23:36:49 +0530

    for else implementation

--- app.py
+++ app.py
@@ -1,13 +1,23 @@
 
 # This code checks if the list name has any name starting with 'J' and prints it.
 # If no name is found, it prints "No name found".
-names = ["pohn", "pane", "Doe"]
+# names = ["pohn", "pane", "Doe"]
 
-found = False
+# found = False
+# for name in names:
+#     if name[0] == 'J':
+#         print(name)
+#         found = True
+#         break
+# if not found:
+#     print("No name found")
+
+# another way
+names = ["pohn", "aane", "Doe"]
+
 for name in names:
-    if name[0] == 'J':
+    if name.startswith('J'):
         print(name)
-        found = True
         break
-if not found:
+else:
     print("No name found")
