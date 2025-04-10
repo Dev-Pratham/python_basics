0dca7e5400d81411d458c2764e77b563c1d3f8d3
Dev-Pratham <pratham.chaurasia31@gmail.com>
2025-04-11 02:28:39 +0530

    Finished basics and learnt to use debugger

--- app.py
+++ app.py
@@ -23,9 +23,40 @@
 #     print("No name found")
 
 # Program that takes input until 5 is entered from user
-guess = None
+# guess = None
 
-while guess != 5:
-    guess = int(input("num : "))
-else:
-    print("You entered 5")
+# while guess != 5:
+#     guess = int(input("num : "))
+# else:
+#     print("You entered 5")
+
+
+# def increment(number, by):
+#     return number + by
+
+
+# # here by is a keyword argument and it helps to understand the arguement being passed
+# # to the function
+# print(increment(5, by=2))
+
+# passing variable number of arguments to a function
+# when we add aestrisk python will automatically pack the arguments into a tuple
+# def multiply(*lists):
+#     product = 1
+#     for list in lists:
+#         product *= list
+#     print(product)
+
+
+# multiply(1, 2, 3, 4, 5)
+
+
+# Variation of multiple arguments
+def user(**User):
+    # this will pack the arguments into a dictionary
+    # and we can access the values using the keys
+    print(User)
+    print(User["name"])
+
+
+user(name="John", age=30, city="New York")
